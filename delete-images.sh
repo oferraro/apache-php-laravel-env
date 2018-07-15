a53ce6b030c2d76acb54c8d269d1baeec177ce90
Oscar Ferraro <oscar@oferraro.oferraro.local>
2018-07-16 00:13:21 +0200

    Add delete volumes inside user confirmation if (remove unnecessary and of if before delete)

--- delete-images.sh
+++ delete-images.sh
@@ -6,7 +6,7 @@ MYSQL_IMG=$4
 VOLUME_DB=$5
 
 deleteContainerAndImg() {
-    if [ "$1" != "" ] && [ "$CONT_ID" != "" ]; then
+    if [ "$1" != "" ]; then
         echo "Stopping the containr $1 ... "
         docker stop $1
         echo "Deleting the containr $1 ... "
@@ -26,10 +26,10 @@ WEB_ID=$(docker ps -a | grep -i "$WEB_CONT" | cut -d " " -f 1)
 	if [ "$RESP" = "yes" ]; then 
 		deleteContainerAndImg "$WEB_ID" "$WEB_IMG"
         deleteContainerAndImg "$MYSQL_CONT" "$MYSQL_IMG"
-	fi
 
 DB_VOL_ID=$(docker volume  ls | grep "$VOLUME_DB" | awk '{print $2}')
-    if [ "$DB_VOL_ID" != "" ]; then
-        echo "Deleting $DB_VOL_ID volume..."
-        docker volume rm $DB_VOL_ID
+        if [ "$DB_VOL_ID" != "" ]; then
+            echo "Deleting $DB_VOL_ID volume..."
+            docker volume rm $DB_VOL_ID
+        fi
     fi
